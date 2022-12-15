b5d1dbc731546530a9226e7736e486f28b426353
king <moshlingcat2004@gmail.com>
2022-12-15 14:45:53 -0500

    V1.0.1

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  success: (response = "Success!", json = {}) => ({
+    status: true,
+    response,
+    ...json
+  }),
+  err: (response = "Error!", json = {}) => ({
+    status: false,
+    response,
+    ...json
+  })
+}
